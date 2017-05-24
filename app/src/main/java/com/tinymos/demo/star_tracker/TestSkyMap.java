38106951068fdfd9e08c9f662e24c012f2a3c5b4
zhanghao <zhangghaoo@gmail.com>
2017-05-24 13:00:44 +0800

    add test sky

---
+++ app/src/main/java/com/tinymos/demo/star_tracker/TestSkyMap.java
@@ -0,0 +1,19 @@
+package com.tinymos.demo.star_tracker;
+
+
+import java.io.IOException;
+
+/**
+ * Created by zhanghao on 2017/5/24.
+ */
+
+public class TestSkyMap {
+    public static void main(String[] args) throws IOException {
+//        HttpResponse response;
+//        response = Request.Post("http://www.example.com").bodyForm(Form.form().add("username", "John").add("password", "pass").build()).execute().returnResponse();
+//
+//        System.out.println(response.toString());
+//
+        System.out.println("Hello");
+    }
+}
